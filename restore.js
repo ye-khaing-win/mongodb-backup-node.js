2eeddfa1918d1e107e4b8935217e65242ef188e7
Ye Khaing Win <yekhaingwin.dev@gmail.com>
2022-09-30 14:43:58 +0630

    initial commit

---
+++ restore.js
@@ -0,0 +1,35 @@
+// mongorestore --db=toss-mip --archive=./toss-mip.gzip --gzip
+
+const { spawn } = require("child_process");
+const path = require("path");
+const cron = require("node-cron");
+
+const DB = "toss-mip";
+const ARCHIVE_PATH = path.join(__dirname, "backup_data", `${DB}.gzip`);
+
+const restoreMnogoDB = function () {
+  const child = spawn("mongorestore", [
+    `--db=${DB}`,
+    `--archive=${ARCHIVE_PATH}`,
+    "--gzip",
+  ]);
+
+  child.stdout.on("data", (data) => {
+    console.log("stdout:\n", data);
+  });
+  child.stderr.on("data", (data) => {
+    console.log("stderr:\n", Buffer.from(data).toString());
+  });
+
+  child.on("error", (error) => {
+    console.log("error:\n", error);
+  });
+
+  child.on("exit", (code, signal) => {
+    if (code) console.log("Process exit with code:", code);
+    else if (signal) console.log("Process killed with signal:", signal);
+    else console.log("Backup is successful");
+  });
+};
+
+restoreMnogoDB();
